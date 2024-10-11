7d36a24048395ba545cf0c01d3df4b792534b29f
wasabina67 <wasabina67@gmail.com>
2024-10-12 00:09:58 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,14 @@
+import os
+
+from dotenv import load_dotenv
+from linebot.v3 import messaging
+
+load_dotenv(override=True)
+
+
 def main():
-    pass
+    user_id = os.getenv("LINE_USER_ID")
+    access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
 
 
 if __name__ == "__main__":
