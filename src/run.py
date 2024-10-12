4e5a5a6ef9eb40df3094b66707fc0dd5065e216c
wasabina67 <wasabina67@gmail.com>
2024-10-12 09:25:34 +0900

    Update run.py

--- src/run.py
+++ src/run.py
@@ -8,7 +8,7 @@ from linebot.v3 import messaging  # type: ignore
 load_dotenv(override=True)
 
 
-def main(timing):
+def main(timing, broadcast=False):
     user_id = os.getenv("LINE_USER_ID")
     access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
     messages_by_timing = {
@@ -25,22 +25,32 @@ def main(timing):
     }
 
     configuration = messaging.Configuration(access_token=access_token)
-    message_dict = {
-        "to": user_id,
-        "messages": [
-            {"type": "text", "text": random.choice(messages_by_timing[timing])},
-        ],
-    }
+    if broadcast:
+        message_dict = {
+            "messages": [
+                {"type": "text", "text": random.choice(messages_by_timing[timing])},
+            ],
+        }
+    else:
+        message_dict = {
+            "to": user_id,
+            "messages": [
+                {"type": "text", "text": random.choice(messages_by_timing[timing])},
+            ],
+        }
 
     with messaging.ApiClient(configuration=configuration) as client:
         messaging_api = messaging.MessagingApi(client)
-        push_message_request = messaging.PushMessageRequest.from_dict(obj=message_dict)
+        if broadcast:
+            pass
+        else:
+            push_message_request = messaging.PushMessageRequest.from_dict(obj=message_dict)
 
-        try:
-            resp = messaging_api.push_message(push_message_request)  # noqa
-            # print(resp)
-        except Exception as e:
-            print(e)
+            try:
+                resp = messaging_api.push_message(push_message_request)  # noqa
+                # print(resp)
+            except Exception as e:
+                print(e)
 
 
 if __name__ == "__main__":
